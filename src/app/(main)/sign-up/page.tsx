33fee72ee6a05716c5085d7023010155b041440f
Sasha Kuharchuk <oleksakukh@gmail.com>
2025-08-05 23:39:16 +0300

    added sign-up page

---
+++ src/app/(main)/sign-up/page.tsx
@@ -0,0 +1,59 @@
+import { JSX } from 'react';
+import Link from 'next/link';
+
+import { Button } from '@/shared/components/Button/Button';
+
+import HomeIcon from 'public/icons/house-outline.svg';
+import ToolIcon from 'public/icons/tabler_tool.svg';
+
+export default function SignUp(): JSX.Element {
+    return (
+        <main
+            className="flex h-[890px] bg-cover justify-end"
+            style={{
+                backgroundImage: "url('/images/hero.png')",
+            }}
+        >
+            <section className="bg-[#FFFFFF] mt-[192px] flex flex-col rounded-[20px] p-16 h-max w-[578px] gap-5 mr-[100px]">
+                <div className="flex flex-col gap-4 text-[#242424]">
+                    <h1 className="font-[500] text-[84px]">Sign Up</h1>
+                    <p className="text-[20px] font-[400] ">
+                        Choose your role to get started:
+                        <br />
+                        Homeowner – post your project and find trusted
+                        contractors. Contractor – browse available jobs and
+                        connect with homeowners.
+                    </p>
+                </div>
+                <nav className="flex gap-4 mt-6" aria-label="Choose role">
+                    <Link href="/sign-up/next-step">
+                        <Button
+                            type="button"
+                            variant="solid"
+                            color="primary"
+                            icon={<HomeIcon />}
+                            iconPosition="left"
+                            className="!bg-[#003BFF] !h-[44px] !px-6 !py-3 !w-full justify-center"
+                            aria-label="Sign up as Homeowner"
+                        >
+                            I’m Homeowner
+                        </Button>
+                    </Link>
+                    <Link href="/sign-up/next-step">
+                        <Button
+                            type="button"
+                            variant="outline"
+                            color="primary"
+                            icon={<ToolIcon />}
+                            iconPosition="left"
+                            className="!border-[#003BFF] !text-[#003BFF] !h-[44px] !px-6 !py-3 !w-full justify-center"
+                            aria-label="Sign up as Contractor"
+                        >
+                            I’m Contractor
+                        </Button>
+                    </Link>
+                </nav>
+            </section>
+        </main>
+    );
+}
