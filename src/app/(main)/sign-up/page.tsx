faf1d6746393125873d525eaf2774c6ac320999b
Sasha Kuharchuk <oleksakukh@gmail.com>
2025-08-06 18:53:42 +0300

    fix

--- src/app/(main)/sign-up/page.tsx
+++ src/app/(main)/sign-up/page.tsx
@@ -11,49 +11,57 @@ export default function SignUp(): JSX.Element {
         <main
             className="flex h-[890px] bg-cover justify-end"
             style={{
-                backgroundImage: "url('/images/hero.png')",
+                backgroundImage: "url('/images/login-hero.png')",
             }}
         >
-            <section className="bg-[#FFFFFF] mt-[192px] flex flex-col rounded-[20px] p-16 h-max w-[578px] gap-5 mr-[100px]">
-                <div className="flex flex-col gap-4 text-[#242424]">
-                    <h1 className="font-[500] text-[84px]">Sign Up</h1>
-                    <p className="text-[20px] font-[400] ">
-                        Choose your role to get started:
-                        <br />
-                        Homeowner – post your project and find trusted
-                        contractors. Contractor – browse available jobs and
-                        connect with homeowners.
-                    </p>
-                </div>
-                <nav className="flex gap-4 mt-6" aria-label="Choose role">
-                    <Link href="/sign-up/next-step">
-                        <Button
-                            type="button"
-                            variant="solid"
-                            color="primary"
-                            icon={<HomeIcon />}
-                            iconPosition="left"
-                            className="!bg-[#003BFF] !h-[44px] !px-6 !py-3 !w-full justify-center"
-                            aria-label="Sign up as Homeowner"
+            <div className="max-w-[1240px] mx-auto w-full flex justify-end mt-[192px]">
+                <section className="bg-[#FFFFFF]  flex flex-col rounded-[20px] p-16 h-max w-[578px] gap-5">
+                    <div className="flex flex-col gap-4 text-[#242424]">
+                        <h1 className="font-[500] text-[84px]">Sign Up</h1>
+                        <p className="text-[20px] font-[400] ">
+                            Choose your role to get started:
+                            <br />
+                            Homeowner – post your project and find trusted
+                            contractors. Contractor – browse available jobs and
+                            connect with homeowners.
+                        </p>
+                    </div>
+                    <nav className="flex gap-4 mt-6" aria-label="Choose role">
+                        <Link
+                            href="/sign-up/next-step"
+                            className="w-full justify-center"
                         >
-                            I’m Homeowner
-                        </Button>
-                    </Link>
-                    <Link href="/sign-up/next-step">
-                        <Button
-                            type="button"
-                            variant="outline"
-                            color="primary"
-                            icon={<ToolIcon />}
-                            iconPosition="left"
-                            className="!border-[#003BFF] !text-[#003BFF] !h-[44px] !px-6 !py-3 !w-full justify-center"
-                            aria-label="Sign up as Contractor"
+                            <Button
+                                type="button"
+                                variant="solid"
+                                color="primary"
+                                icon={<HomeIcon />}
+                                iconPosition="left"
+                                className="!bg-[#003BFF] !h-[44px] !px-6 !py-3 !w-full justify-center"
+                                aria-label="Sign up as Homeowner"
+                            >
+                                I’m Homeowner
+                            </Button>
+                        </Link>
+                        <Link
+                            href="/sign-up/next-step"
+                            className="w-full justify-center"
                         >
-                            I’m Contractor
-                        </Button>
-                    </Link>
-                </nav>
-            </section>
+                            <Button
+                                type="button"
+                                variant="outline"
+                                color="primary"
+                                icon={<ToolIcon />}
+                                iconPosition="left"
+                                className="!border-[#003BFF] !text-[#003BFF] !h-[44px] !px-6 !py-3 !w-full justify-center"
+                                aria-label="Sign up as Contractor"
+                            >
+                                I’m Contractor
+                            </Button>
+                        </Link>
+                    </nav>
+                </section>
+            </div>
         </main>
     );
 }
